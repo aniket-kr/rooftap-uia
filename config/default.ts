2b482ea26e8af2c75ed1a0f0bacc57c8020fef47
Aniket Kumar <aniket24kr@gmail.com>
2022-11-16 17:25:45 +0530

    Setup skeleton

---
+++ config/default.ts
@@ -0,0 +1,4 @@
+export default {
+  serverPort: 8080,
+  dbUri: 'db_uri_goes_here',
+} as const;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+import config from 'config';
 import express from 'express';
+import baseRouter from '@src/routes/index';
 
 const app = express();
 
-app.listen();
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'pug');
+
+// register all routes
+app.use('/', baseRouter);
+
+app.listen(config.get<number>('serverPort'), () => {
+  console.log('Server listening!');
+});
