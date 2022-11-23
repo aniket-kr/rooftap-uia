f107cb980fe39bfc720af8e0f15199dfa516eb9c
Aniket Kumar <aniket24kr@gmail.com>
2022-11-23 23:25:33 +0530

    Fix leaked Maps Key

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,7 @@
+import 'dotenv/config';
+
 export default {
   serverPort: 8080,
   dbUri: 'mongodb://127.0.0.1:27017/rooftap',
+  mapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
 } as const;
--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
+import config from 'config';
 
 export function indexHandler(req: Request, res: Response, next: NextFunction) {
-  res.render('index', {});
+  res.render('index', { mapsApiKey: config.get<string>('mapsApiKey') });
 }
